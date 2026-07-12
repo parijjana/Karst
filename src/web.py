d30ce36bcdae7164c396875c1f2a910f05744355
parijjana <159910152+parijjana@users.noreply.github.com>
2026-07-12 20:36:04 +0530

    Change port to 8085 to avoid TIME_WAIT socket lock

--- src/web.py
+++ src/web.py
@@ -222,4 +222,4 @@ async def get_graph(project_id: int = None):
         return {"nodes": nodes, "links": links}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8085)
